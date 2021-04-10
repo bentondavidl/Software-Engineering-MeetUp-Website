c12c3e3e233ded5bd55e389d1146f9c0a1a7d894
David <bentondavidl@users.noreply.github.com>
2021-04-09 22:07:56 -0400

    set up skeleton for database files

---
+++ database/models.py
@@ -0,0 +1,21 @@
+from database import db
+
+class User(db.Model):
+    # add records here
+
+    # add parameters for each field
+    def __init__(self):
+        pass
+
+class Event(db.Model):
+    # add records here
+
+    # add parameters for each field
+    def __init__(self):
+        pass
+
+class RSVP(db.Model):
+    # same as above
+    
+    pass
+
