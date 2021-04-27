ddb11713ee64c335ffe43fe1dd44bbb3624d02c5
David <bentondavidl@users.noreply.github.com>
2021-04-27 14:20:19 -0400

    add documentation to models.py

--- database/models.py
+++ database/models.py
@@ -1,3 +1,5 @@
+"""Definition of database tables"""
+
 from database.database import db
 
 class User(db.Model):
@@ -11,6 +13,19 @@ class User(db.Model):
     rsvps = db.relationship('RSVP', backref='user', lazy=True)
     # add parameters for each field
     def __init__(self, first_name, last_name, password, email):
+        """Create User instance
+
+        Parameters
+        ----------
+        first_name : str
+            User's first name
+        last_name : str
+            User's last name
+        password : str
+            User's hashed password
+        email : str
+            Email address of the user
+        """
         self.first_name = first_name
         self.last_name = last_name 
         self.password = password
@@ -29,6 +44,25 @@ class Event(db.Model):
     rsvps = db.relationship('RSVP', backref='event', cascade='all, delete-orphan', lazy=True)
     # add parameters for each field
     def __init__(self, host_id, name, is_private, location, start_time, end_time, description):
+        """Create Event record
+
+        Parameters
+        ----------
+        host_id : int
+            User ID of the event host/creator
+        name : str
+            Name of the event
+        is_private : bool
+            True if private, False if public
+        location : str
+            Where the event is to be held
+        start_time : datetime
+            Day and time for event to start
+        end_time : datetime
+            Day and time for event to end
+        description : str
+            Description of the event
+        """
         self.host_id = host_id
         self.name = name
         self.is_private = is_private
@@ -47,6 +81,19 @@ class RSVP(db.Model):
     guests = db.Column('guests', db.SmallInteger, nullable=False)
 
     def __init__(self, user_id, event_id, is_going, guests=0):
+        """Create RSVP instance
+
+        Parameters
+        ----------
+        user_id : int
+            User that is responding to the event
+        event_id : int
+            ID of the event being responded to
+        is_going : bool
+            True if they are going, False if they are not
+        guests : int, optional
+            number of guests, not including respondant, attending the event, by default 0
+        """
         self.user_id = user_id
         self.event_id = event_id
         self.is_going = is_going
